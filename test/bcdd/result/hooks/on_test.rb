2e218e02bbda2163ee0d9d5a07b908871efa181c
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2023-09-23 11:01:17 -0300

    Improve BCDD::Result#on tests

--- test/bcdd/result/hooks/on_test.rb
+++ test/bcdd/result/hooks/on_test.rb
@@ -4,12 +4,30 @@ require 'test_helper'
 
 module BCDD::Result
   class OnHookTest < Minitest::Test
-    test '#on' do
+    test '#on returns the result itself' do
       result = Success.new(type: :okay, value: 1)
 
-      r = result.on(:okay) { |value| assert_equal(1, value) }
+      r1 = result.on(:okay) { |value| assert_equal(1, value) }
 
-      assert_same result, r
+      r2 =
+        result
+        .on(:okay) { |value| assert_equal(1, value) }
+        .on(:okay) { |value| assert_equal(1, value) }
+
+      assert_same result, r1
+      assert_same result, r2
+    end
+
+    test '#on works with successes or failures' do
+      number = 0
+
+      success = Success.new(type: :foo, value: 1)
+      failure = Failure.new(type: :foo, value: 2)
+
+      success.on(:foo) { |value| number += value }
+      failure.on(:foo) { |value| number += value }
+
+      assert_equal 3, number
     end
 
     test '#on without receving types/arguments' do
@@ -58,18 +76,6 @@ module BCDD::Result
         .on(:err) { |value| number += value }
 
       assert_equal 0, number
-    end
-
-    test '#on matches the type independent of the result kind' do
-      number = 0
-
-      success = Success.new(type: :foo, value: 1)
-      failure = Failure.new(type: :foo, value: 2)
-
-      success.on(:foo) { |value| number += value }
-      failure.on(:foo) { |value| number += value }
-
-      assert_equal 3, number
     end
   end
 end
