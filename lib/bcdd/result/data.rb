d4e3dec8d64d069f3c223ac356fd3cf867d09c82
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2023-09-28 23:13:30 -0300

    Make BCDD::Result::Data be private

--- lib/bcdd/result/data.rb
+++ lib/bcdd/result/data.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
-class BCDD::Result::Data
-  attr_reader :name, :type, :value, :to_h, :to_a
+class BCDD::Result
+  class Data
+    attr_reader :name, :type, :value, :to_h, :to_a
 
-  def initialize(result)
-    @name = result.send(:name)
-    @type = result.type
-    @value = result.value
+    def initialize(result)
+      @name = result.send(:name)
+      @type = result.type
+      @value = result.value
 
-    @to_h = { name: name, type: type, value: value }
-    @to_a = [name, type, value]
+      @to_h = { name: name, type: type, value: value }
+      @to_a = [name, type, value]
+    end
+
+    def inspect
+      format(
+        '#<%<class_name>s name=%<name>p type=%<type>p value=%<value>p>',
+        class_name: self.class.name, name: name, type: type, value: value
+      )
+    end
+
+    alias to_ary to_a
+    alias to_hash to_h
   end
 
-  def inspect
-    format(
-      '#<%<class_name>s name=%<name>p type=%<type>p value=%<value>p>',
-      class_name: self.class.name, name: name, type: type, value: value
-    )
-  end
-
-  alias to_ary to_a
-  alias to_hash to_h
+  private_constant :Data
 end
