4b29664051ff7aaed493d49b450dd8c97eb15d4c
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2023-10-11 00:36:22 -0300

    Add BCDD::Result.mixin(with: :Continue)

--- lib/bcdd/result/mixin.rb
+++ lib/bcdd/result/mixin.rb
@@ -12,9 +12,27 @@ class BCDD::Result
       end
     end
 
+    module Addons
+      module Continuable
+        def Continue(value)
+          Success(:continued, value)
+        end
+      end
+
+      OPTIONS = { Continue: Continuable }.freeze
+
+      def self.options(names)
+        Array(names).filter_map { |name| OPTIONS[name] }
+      end
+    end
+  end
+
   def self.mixin(with: nil)
+    addons = Mixin::Addons.options(with)
+
     mod = Module.new
     mod.send(:include, Mixin::Methods)
+    mod.send(:include, *addons) unless addons.empty?
     mod
   end
 
