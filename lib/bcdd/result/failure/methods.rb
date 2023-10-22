0d2faf9ff66c75dc0e3570b48f7c3f5da868c356
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2023-10-22 16:10:01 -0300

    Add BCDD::Result::Failure::Methods

---
+++ lib/bcdd/result/failure/methods.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module BCDD::Result::Failure::Methods
+  def success?(_type = nil)
+    false
+  end
+
+  def failure?(type = nil)
+    type.nil? || type_checker.allow_failure?([type])
+  end
+
+  def value_or
+    yield
+  end
+
+  private
+
+  def name
+    :failure
+  end
+end
