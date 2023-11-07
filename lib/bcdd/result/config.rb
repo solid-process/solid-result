6779e140c06333bced8e65ac12d1faf74a5b790c
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2023-11-06 23:36:26 -0300

    Refactor BCDD::Result::Config

--- lib/bcdd/result/config.rb
+++ lib/bcdd/result/config.rb
@@ -2,18 +2,32 @@
 
 require 'singleton'
 
+require_relative 'config/switcher'
+require_relative 'config/constant_alias'
+
 class BCDD::Result
   class Config
-    require_relative 'config/constant_alias'
-    require_relative 'config/pattern_matching'
-
     include Singleton
 
-    attr_reader :pattern_matching, :constant_alias
+    CONSTANT_ALIAS = Switcher.factory(
+      options: ConstantAlias::OPTIONS,
+      listener: ConstantAlias::Listener
+    )
+
+    PATTERN_MATCHING = Switcher.factory(
+      options: {
+        nil_as_valid_value_checking: %w[
+          BCDD::Result::Expectations
+          BCDD::Result::Context::Expectations
+        ]
+      }
+    )
+
+    attr_reader :constant_alias, :pattern_matching
 
     def initialize
-      @constant_alias = ConstantAlias.new
-      @pattern_matching = PatternMatching.new
+      @constant_alias = CONSTANT_ALIAS.call
+      @pattern_matching = PATTERN_MATCHING.call
     end
 
     def freeze
--- lib/bcdd/result/config/constant_alias.rb
+++ lib/bcdd/result/config/constant_alias.rb
@@ -2,86 +2,25 @@
 
 class BCDD::Result
   class Config
-    class ConstantAlias
-      module Option
-        LIST = [
-          RESULT = 'Result'
-        ].freeze
+    module ConstantAlias
+      RESULT = 'Result'
 
-        AFFECTS = {
-          RESULT => %w[Object]
-        }.transform_values!(&:freeze).freeze
+      OPTIONS = {
+        RESULT => %w[Object]
+      }.transform_values!(&:freeze).freeze
 
-        MAPPING = {
-          RESULT => { target: ::Object, name: :Result, value: ::BCDD::Result }
-        }.transform_values!(&:freeze).freeze
-      end
+      MAPPING = {
+        RESULT => { target: ::Object, name: :Result, value: ::BCDD::Result }
+      }.transform_values!(&:freeze).freeze
 
-      attr_reader :_options
-
-      private :_options
-
-      def initialize
-        @_options = Option::LIST.to_h { [_1, false] }
-      end
-
-      def freeze
-        _options.freeze
-        super
-      end
-
-      def options
-        Option::AFFECTS.to_h { |name, affects| [name, { enabled: _options[name], affects: affects }] }
-      end
-
-      def enabled?(name)
-        _options[name] || false
-      end
-
-      def enable!(*names)
-        set_many(names, to: true)
-      end
-
-      def disable!(*names)
-        set_many(names, to: false)
-      end
-
-      private
-
-      def set_many(names, to:)
-        option_required!(names)
-
-        names.each { |name| set_one(name, to) }
-
-        options.slice(*names)
-      end
-
-      def set_one(option_name, boolean)
-        mapping = Option::MAPPING.fetch(option_name)
+      Listener = ->(option_name, boolean) do
+        mapping = MAPPING.fetch(option_name)
 
         target, name, value = mapping.fetch_values(:target, :name, :value)
 
-        set_one!(boolean, target: target, name: name, value: value)
-
-        _options[name] = boolean
-      rescue ::KeyError
-        option_invalid!(name)
-      end
-
-      def set_one!(boolean, target:, name:, value:)
         defined = target.const_defined?(name, false)
 
         boolean ? defined || target.const_set(name, value) : defined && target.send(:remove_const, name)
-      end
-
-      AVAILABLE_OPTIONS = "Available options: #{Option::LIST.map(&:inspect).join(', ')}"
-
-      def option_required!(names)
-        raise ::ArgumentError, "One or more options required. #{AVAILABLE_OPTIONS}" if names.empty?
-      end
-
-      def option_invalid!(name)
-        raise ::ArgumentError, "Invalid option: #{name.inspect}. #{AVAILABLE_OPTIONS}"
       end
     end
 
