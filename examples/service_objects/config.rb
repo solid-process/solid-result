066abd98e3da509ad44475c1613ef34f625162c9
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-02-20 09:30:56 -0300

    Add examples/service_objects

---
+++ examples/service_objects/config.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'bundler/inline'
+
+$LOAD_PATH.unshift(__dir__)
+
+require_relative 'config/boot'
+require_relative 'config/initializers/bcdd'
+
+require 'db/setup'
+
+require 'app/models/account'
+require 'app/models/account/member'
+require 'app/models/user'
+require 'app/models/user/token'
+
+require 'app/services/application_service'
+require 'app/services/account/owner_creation'
+require 'app/services/user/token/creation'
+require 'app/services/user/creation'
