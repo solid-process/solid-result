15c9a79b27328ef59a12688c70b1ae32f3628798
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-03-16 09:14:32 -0300

    Update examples

--- examples/multiple_listeners/config/initializers/bcdd.rb
+++ examples/multiple_listeners/config/initializers/bcdd.rb
@@ -3,8 +3,6 @@
 BCDD::Result.config.then do |config|
   config.addon.enable!(:continue)
 
-  config.constant_alias.enable!('BCDD::Context')
-
   config.pattern_matching.disable!(:nil_as_valid_value_checking)
 
   # config.feature.disable!(:expectations) if Rails.env.production?
